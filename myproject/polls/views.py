c5050ce97a3e2a610e93c0c8cf20948d24a56a52
Ehsan Shahbazi <meshahbazi72@gmail.com>
2020-07-12 14:47:57 +0430

    The sell solved.

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 import numpy as np
+from .models import Trader
+
 # Create your views here.
+
+
+def home(request):
+    traders = Trader.objects.all()
+    the_trader = traders[0]
+    # todo: find the correct user when we have multiple users
+    activities = the_trader.activity_set.all()
+    context = {
+        "activity": activities
+    }
+    return render(request, 'polls/plot.html', context=context)
