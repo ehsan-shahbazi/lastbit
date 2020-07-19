f62ccde13f99422e1fffe8472983be6f57920f52
Ehsan Shahbazi <meshahbazi72@gmail.com>
2020-07-19 19:10:53 +0430

    plot is done

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,16 +1,43 @@
 from django.shortcuts import render
 import numpy as np
-from .models import Trader
+from .models import Activity
 
 # Create your views here.
 
 
 def home(request):
-    traders = Trader.objects.all()
-    the_trader = traders[0]
-    # todo: find the correct user when we have multiple users
-    activities = the_trader.activity_set.all()
+    activities = list(Activity.objects.all())
+    labels = [i for i in range(len(activities))]
+    data = [act.price for act in activities]
+    asset = []
+    mat = 0
+    budget = 0
+    for act in activities:
+        if act == activities[0]:
+            asset.append(act.price)
+            if act.action == 'buy':
+                mat = 1
+            else:
+                budget = act.price
+            continue
+        if act.action == 'buy':
+            if budget == 0:
+                asset.append(mat * act.price)
+            else:
+                asset.append(budget * 0.999)
+                mat = budget * 0.999 / act.price
+                budget = 0
+        else:
+            if mat == 0:
+                asset.append(budget)
+            else:
+                asset.append(mat * 0.999 * act.price)
+                budget = mat * 0.999 * act.price
+                mat = 0
+
     context = {
-        "activity": activities
+        "labels": labels,
+        "data": data,
+        "asset": asset
     }
-    return render(request, 'polls/plot.html', context=context)
+    return render(request, 'polls/plots.html', context=context)
