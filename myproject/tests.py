fbdad3664442975c102e30b6c73d481d8c6e88e0
Ehsan Shahbazi <meshahbazi72@gmail.com>
2021-03-30 22:34:49 +0430

    working on stable buy and sell

--- myproject/tests.py
+++ myproject/tests.py
@@ -6,11 +6,12 @@ warnings.filterwarnings("ignore")
 file_location = ''
 os.environ["DJANGO_SETTINGS_MODULE"] = 'myproject.settings'
 django.setup()
-from polls.models import User, Material, Finance
+from polls.models import User, Finance
 name = 'mahsa'
-user = User.objects.get(name='name')
+user = User.objects.get(name=name)
 finance = Finance.objects.get(user=user)
+print(finance)
+
 finance.finish_margin()
 finance.buy(price=finance.get_price())
 finance.long_buy(portion=1)
-print(finance)
